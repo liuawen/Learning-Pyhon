77a3b14537b9afda0b713d8f4c76cd950efa596a
Liu Awen <46917165+liuawen@users.noreply.github.com>
2019-07-04 12:45:58 +0800

    Add files via upload

---
+++ Python quick start 快速入门/2.2 Python 基本编程解析（中）/loop statement.py
@@ -0,0 +1,11 @@
+#Traversal cycle  遍历循环
+#遍历循环：从遍历结构中逐一提取元素，放入循环变量
+#输出0 1 2 3 4
+for i in range(5):
+    print(i)
+#infinite loop 无限循环
+# 无限循环：反复执行语句快，直到条件不满足时结束
+#输出四到九
+while i<10:
+    print(i)
+    i += 1
