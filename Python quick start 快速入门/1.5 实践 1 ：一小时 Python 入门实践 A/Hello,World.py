7c3b27f223b44c0fd4e8c9401829ebdf7ba0615f
willowawen@gmail.com <willowawen@gmail.com>
2019-07-07 17:38:53 +0800

    删除了Python quick start

--- Python quick start 快速入门/1.5 实践 1 ：一小时 Python 入门实践 A/Hello,World.py
+++
@@ -1,2 +0,0 @@
-print("世界，你好！")
-print("Python,我来了!")
--- Python quick start 快速入门/2.2 Python 基本编程解析（中）/expection handing.py
+++
@@ -1,8 +0,0 @@
-#异常处理：对程序执行异常的处理机制，提高用户体验
-try:
-    f = open("sensor-data-no-exist.txt", "r")
-    for line in f:
-        print(line)
-    f.close()
-except:
-    print("文件不存在")
